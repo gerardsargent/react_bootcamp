d65cf316a63ae29192cb697d5b7670b9637c3630
Gerard Sargent <gerard.sargent@calligo.cloud>
2019-07-20 16:04:01 +0100

    Add Deck.css Convert card into object for state Add getNewPack function for restarting game Add random rotation for cards Remove loading state from component Delete spinner.gif

--- 016a_yahtzee_starter/src/RuleRow.jsx
+++ 016a_yahtzee_starter/src/RuleRow.jsx
@@ -4,7 +4,7 @@ import './RuleRow.css'
 class RuleRow extends Component {
   render() {
     return (
-      <tr className={`RuleRow ${this.props.score == null ? "RuleRow-active" : "RuleRow-disabled"}`} onClick={this.props.doScore}>
+      <tr className={`RuleRow ${this.props.score == null ? "RuleRow-active" : "RuleRow-disabled"}`} onClick={this.props.score == null ? this.props.doScore : null}>
         <td className="RuleRow-name">{this.props.name}</td>
         <td className="RuleRow-score">{this.props.score == null ? this.props.description : this.props.score}</td>
       </tr>
--- 017_deck_of_cards/src/Deck.jsx
+++ 017_deck_of_cards/src/Deck.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import axios from 'axios'
-import Spinner from './img/spinner.gif'
+import './Deck.css'
 
 export default class Deck extends Component {
   constructor(props) {
@@ -8,49 +8,88 @@ export default class Deck extends Component {
 
     this.state = {
       deck_id: '',
-      cardData: {},
-      loading: false,
+      cardsPulled: [],
+      cardsRemaining: 52,
       gameOver: false
     }
 
     this.drawCard = this.drawCard.bind(this)
+    this.generateRandomNumber = this.generateRandomNumber.bind(this)
+    this.getNewPack = this.getNewPack.bind(this)
   }
 
   componentDidMount () {
+    this.getNewPack()
+  }
+
+  getNewPack () {
     axios.get('https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1')
-      .then(response => {
-        this.setState({
-          deck_id: response.data.deck_id
-        })
+    .then(response => {
+      this.setState({
+        deck_id: response.data.deck_id
       })
+    })
+
+    this.setState({
+      deck_id: '',
+      cardsPulled: [],
+      cardsRemaining: 52,
+      gameOver: false
+    })
   }
 
   drawCard () {
-    const { deck_id } = this.state
-
-    this.setState ({ loading: true })
+    const { deck_id, cardsPulled } = this.state
+    const { generateRandomNumber } = this
 
     axios.get(`https://deckofcardsapi.com/api/deck/${deck_id}/draw/?count=1`)
-      .then(response => {
-        this.setState({
-          cardData: response,
-          loading: false
-        })
+    .then(response => {
+      const card = response.data.cards[0]
+      const suit = card.suit.toLowerCase()
+      const value = card.value.toLowerCase()
+      const cardImage = card.image
+
+      const newCardObject = {
+        image: cardImage,
+        rotationValue: generateRandomNumber(),
+        description: `The ${value} of ${suit}`
+      }
+
+      const newCardsPulled = [...cardsPulled, newCardObject]
+
+      this.setState({
+        cardsPulled: newCardsPulled,
+        cardsRemaining: response.data.remaining
       })
+    })
+  }
+
+  generateRandomNumber() {
+    let num = Math.floor(Math.random()*20) + 1 // generate num between 1 and 5
+    num *= Math.floor(Math.random()*2) === 1 ? 1 : -1 // add minus sign in 50% of cases
+    return num.toString()
   }
 
   render() {
-    const { cardData, loading } = this.state
-    const { drawCard } = this
+    const { cardsPulled, cardsRemaining } = this.state
+    const { drawCard, getNewPack } = this
 
     return (
       <div className="Deck__container">
-        <button onClick={drawCard} class="Deck__button">Gimme a Card!</button>
-        {cardData.data && (
-          <div class="Deck__card-container">
-            {loading ? <img className="Deck__spinner" src={ Spinner } alt="Loading" /> : <img className="Deck__card-img" src={cardData.data.cards[0].image} alt={`${cardData.data.cards[0].value} of ${cardData.data.cards[0].suit}`} />}
+        {cardsRemaining === 0 ?
+          <button onClick={getNewPack} className="Deck__button">Start Again?</button> :
+          <button onClick={drawCard} className="Deck__button">Gimme a Card!</button>}
+          <div className="Deck__card-container">
+            {cardsPulled.map((card, index) => (
+              <img
+                key={index}
+                className="Deck__card-img"
+                src={card.image}
+                alt={card.description}
+                style={{transform: `rotate(${card.rotationValue}deg)`}}
+              />
+            ))}
           </div>
-        )}
       </div>
     )
   }
